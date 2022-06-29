c1fff4a765b9c4c31c2197fb3619de685d8fd2a2
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-06-29 11:18:55 +0530

    Moved guestBook template to resource

--- TODO.md
+++ TODO.md
@@ -1,9 +1,12 @@
 ## TODO
 
-- [ ] Make parse for comments
+- [ ] Decide content type based on extension
 
 ## DONE
 
+- [x] Move guestBook.html template to resources
+- [x] Extract generateGuestBook.js file
+- [x] ~~Make parse for comments~~
 - [x] Show comments in tabular form
 - [x] Make start book html
 - [x] Take a comment from the user
---
+++ src/app/generateGuestBook.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const getTimeStamp = () => {
+  const date = new Date();
+  const day = date.toLocaleDateString();
+  const time = date.toLocaleTimeString();
+  const timeStamp = `${day} ${time}`;
+  return timeStamp;
+};
+
+const generatePostRow = (timeStamp, name, comment) => {
+  const timeStampTag = `<td>${timeStamp}</td>`;
+  const nameTag = `<td>${name}</td>`;
+  const commentTag = `<td>${comment}</td>`;
+  const post = `<tr>${timeStampTag}${nameTag}${commentTag}</tr>`;
+  return post;
+};
+
+const generateCommentsTable = (prevComments) => {
+  const comments = prevComments.map(({ timeStamp, name, comment }) => {
+    return generatePostRow(timeStamp, name, comment);
+  });
+  return `<table class="comments"><tbody>${comments.join('')}</tbody></table>`;
+};
+
+const getContent = (prevComments) => {
+  const comments = generateCommentsTable(prevComments);
+  const template = fs.readFileSync('./resource/guestbook.html', 'utf8');
+  const content = template.replace('__COMMENTS__', comments);
+  return content;
+};
+const pushNewComment = (name, comment, comments) => {
+  const timeStamp = getTimeStamp();
+  const post = { timeStamp, name, comment };
+  comments.push(post);
+  fs.writeFileSync('./data/comments.json', JSON.stringify(comments), 'utf8');
+  return comments;
+};
+
+const toGuestBookParams = (request) => {
+  const post = {};
+  const entries = request.url.searchParams.entries();
+
+  for (const entry of entries) {
+    post[entry[0]] = entry[1];
+  }
+
+  return post;
+};
+
+const generateGuestBook = (request) => {
+  const { name, comment } = toGuestBookParams(request);
+  const comments = JSON.parse(fs.readFileSync('./data/comments.json', 'utf8'));
+  if (name && comment) {
+    pushNewComment(name, comment, comments);
+  }
+  const content = getContent(comments);
+  return content;
+};
+
+exports.generateGuestBook = generateGuestBook;
