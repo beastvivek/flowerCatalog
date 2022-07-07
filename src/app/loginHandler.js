e4a0ffe330276ac3c9cdfed0a5a89dd9aeb730b5
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-07-07 12:00:14 +0530

    Added password-based login

--- src/app/loginHandler.js
+++ src/app/loginHandler.js
@@ -11,8 +11,15 @@ const loginTemplate = () => `<html>
 
     <form action="/login" method="POST">
       <div>
+        <div>
         <label for="username">Username</label>
         <input type="text" name="username" id="name" placeholder="Enter name" required>
+        </div>
+
+        <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Enter password" required>
+        </div>
       </div>
 
       <div class="login-button">
@@ -24,17 +31,34 @@ const loginTemplate = () => `<html>
 
 </html>`;
 
-const createSession = (username) => {
+const createSession = (username, password) => {
   const date = new Date();
-  return { sessionId: date.getTime(), username };
+  return { sessionId: date.getTime(), username, password };
+};
+
+const isValidUser = (username, password) => {
+  const users = [
+    { username: 'vivek', password: 'ek' },
+    { username: 'gayatri', password: 'three' }
+  ];
+
+  return users.find((user) => {
+    return user.username === username && user.password === password;
+  });
 };
 
 const loginHandler = (sessions) => (request, response, next) => {
-  const { method, url: { pathname }, bodyParams: { username } } = request;
+  const { method, url: { pathname }, bodyParams: { username, password } } = request;
 
   if (pathname === '/login' && method === 'POST') {
-    const session = createSession(username);
+    const session = createSession(username, password);
     sessions[session.sessionId] = session;
+
+    if (!isValidUser(username, password)) {
+      response.statusCode = 401;
+      response.end('Unauthorized User');
+      return;
+    }
 
     response.setHeader('set-cookie', `id=${session.sessionId}`);
     response.statusCode = 302;
